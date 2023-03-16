789d6c7d6395dcebc1a476a1e2f175250a22ce6d
Mario Garcia <mandres_garcia@hotmail.com>
2023-03-16 17:17:16 -0500

    Se agregan comentarios

--- Modules/functions.py
+++ Modules/functions.py
@@ -2,62 +2,81 @@
 #_*_ codig: utf8 _*_
 import datetime, json, os, boto3, smtplib, datetime
 from email.message import EmailMessage
-from Modules.constants import *
+from Modules.constants import * #Se importan las constantes definidas en el archivo constants.py
 
+#*****************************************************************************************************************************************************************************************************************************
+#Se define la función llamada SendMail que toma dos argumentos: text y mail_subject.
+#La función tiene como objetivo enviar un correo electrónico con el contenido
+# y el asunto proporcionados a una dirección de correo electrónico especificada.
 def SendMail(text, mail_subject):
-    msg = EmailMessage()
-    msg.set_content(text)
-    msg['Subject'] = mail_subject
-    msg['From'] = 'alarmas-aws@vcmedios.com.co'
-    msg['To'] = [Mail_To]
-    conexion = smtplib.SMTP(host='10.10.122.17', port=25)
-    conexion.ehlo()
-    conexion.send_message(msg)
-    conexion.quit()
-    return
+    msg = EmailMessage() #crea una nueva instancia de la clase EmailMessage() y la asigna a la variable msg. Esta clase se utiliza para crear y enviar mensajes de correo electrónico.
+    msg.set_content(text) #se establece el contenido del correo electrónico como el valor del argumento text que se pasó a la función.
+    msg['Subject'] = mail_subject #establece el asunto del correo electrónico como el valor del argumento mail_subject que se pasó a la función.
+    msg['From'] = 'alarmas-aws@vcmedios.com.co' #Establece la dirección de correo electrónico del remitente como alarmas-aws@vcmedios.com.co.
+    msg['To'] = [Mail_To] #Establece la dirección de correo electrónico del destinatario como el valor de una constante llamada Mail_To.
+    conexion = smtplib.SMTP(host='10.10.122.17', port=25) #Se establece una conexión con un servidor SMTP que se encuentra en la dirección IP 10.10.122.17 y el puerto 25. Esta conexión se asigna a la variable conexion.
+    conexion.ehlo() #Se utiliza el comando EHLO (abreviatura de "Hola extendido") para iniciar una conexión con el servidor SMTP.
+    conexion.send_message(msg) #se envía el mensaje de correo electrónico utilizando la conexión establecida anteriormente.
+    conexion.quit() #se cierra la conexión SMTP.
+    return #devuelve none.
+#*****************************************************************************************************************************************************************************************************************************
 
+#****************************************************************************************************************************************************************
+#Se define la función llamada Download_Logs, que toma un argumento llamado DATE_LOG. 
+#La función tiene como objetivo descargar todos los archivos de registro de un Bucket 
+# de Amazon S3 y guardarlos en una ruta local.
 def Download_Logs(DATE_LOG):
-    objects={'Objects':[]}
-    list_objects=[]
-    aws_session=boto3.Session(profile_name=aws_profile)
-    s3_client=aws_session.client('s3')
-    logs=s3_client.list_objects_v2(Bucket=Bucket_logs)
-    if 'Contents' in logs:    
-        for i in range(len(logs['Contents'])):
-            log_Key=logs['Contents'][i]['Key']
-            with open(f'{Downloads_Path}/{log_Key}', 'wb') as data:
-                s3_client.download_fileobj(Bucket_logs, log_Key, data)
-            objects['Objects'].append({'Key': log_Key,})
-            list_objects.append(f"{Downloads_Path}/{log_Key}")
-            #s3_client.copy_object(
-                # Bucket=Bucket_logs_old,
-                # CopySource=f'{Bucket_logs}/{log_Key}',
-                # Key=f'{log_Key}'
+    objects={'Objects':[]} #Se inicializa la variable como un diccionario vacio.
+    list_objects=[] #Se inicializa la variable como una lista vacia.
+    aws_session=boto3.Session(profile_name=aws_profile) #Se crea una sesion de AWS bajo el perfil definido en la constante aws_profile.
+    s3_client=aws_session.client('s3') #Se establece conexion con el servicio S3.
+    logs=s3_client.list_objects_v2(Bucket=Bucket_logs) #se listan todos los objetos (archivos) en el Bucket definido en la constante Bucket_logs de S3.
+    if 'Contents' in logs: #Se comprueba si hay objetos en el bucket.   
+        for i in range(len(logs['Contents'])): #Si hay objetos se itera sobre cada objeto.
+            log_Key=logs['Contents'][i]['Key'] #Captura de la ruta del objeto.
+            with open(f'{Downloads_Path}/{log_Key}', 'wb') as data: #Se crea un archivo en el folder local.
+                s3_client.download_fileobj(Bucket_logs, log_Key, data) #Se descarga el objeto y se copia el contenido al archivo anteriormente creado.
+            objects['Objects'].append({'Key': log_Key,}) #Se agrega al dicionario el nombre del objeto descargado.
+            list_objects.append(f"{Downloads_Path}/{log_Key}") #Se agrega la ruta completa del objeto descargado en el folder local.
+            #Se realiza un a copia del objeto en el bucket definido en la constante Bucket_logs_old.
+            #s3_client.copy_object( 
+                # Bucket=Bucket_logs_old, #Se establece el bucket de destino.
+                # CopySource=f'{Bucket_logs}/{log_Key}', #Se establece la ruta del objeto en el bucket de origen.
+                # Key=f'{log_Key}' #Se establece el nombre del objeto en el destino.
                 # )
-            
+        #Se eliminan los objetos en el bucket de origen.    
         # s3_client.delete_objects(
-        #         Bucket=Bucket_logs,
-        #         Delete=objects
+        #         Bucket=Bucket_logs, #Se establce el bucket.
+        #         Delete=objects #Se establce la lista de objetos a eliminar.
         #     )
-        return list_objects
+        return list_objects #Retorna la lista de objetos cada uno con su ruta en el folder local.
     else:
-        text_print=f"Logs not found"
-        print_log("a",text_print,DATE_LOG)
-        return []
+        text_print=f"Logs not found" #Se define el texto a registrar en el log de eventos.
+        print_log(text_print,DATE_LOG) #Se registra el texto en el log de eventos.
+        return [] #Retorna lista vacia.
+#*****************************************************************************************************************************************************************
 
-def print_log(OPTION, TEXT, DATE_LOG):
-    log_file=open(f"{log_Path}/{DATE_LOG}_log.txt", OPTION)
-    log_file.write(f"{str(datetime.datetime.strftime(datetime.datetime.now(), '%H:%M:%S'))}\t{TEXT}\n")
-    log_file.close()
+#*********************************************************************************************************************************************************************************
+#Se define la funcion llamada print_log, que toma dos argumentos llamados TEXT, DATE_LOG.
+#La funcion tiene como objetivo registrar en un archivo txt un texto definido en la variable TEXT.
+def print_log(TEXT, DATE_LOG):
+    log_file=open(f"{log_Path}/{DATE_LOG}_log.txt", "a") #Se abre el archivo de la ruta especificada. Si el archivo no existe este se crea. 
+    log_file.write(f"{str(datetime.datetime.strftime(datetime.datetime.now(), '%H:%M:%S'))}\t{TEXT}\n") #Se escribe en el archivo log el texo especificado en el argumento TEXT.
+    log_file.close() #Se cierra el archivo.
+#**********************************************************************************************************************************************************************************
 
+#****************************************************************************************************************
+#Se define la funcion llamada Flag_status, que toma un argumento llamado OPTION.
+#La funcion tiene como objetivo leer y escribir un archivo json.
 def Flag_Status(OPTION):
-    with open(json_path, "r") as json_file:
-            json_data=json.load(json_file)
-    if OPTION=="r":
-        return json_data["FLAG"]
-    elif OPTION=="w":
-        json_data["FLAG"]=False
-        with open(json_path, "w") as json_file:
-            json.dump(json_data, json_file)
+    with open(json_path, "r") as json_file: #Se abre el archivo json especificado en la constante json_path.
+            json_data=json.load(json_file) #Se lee el contenido del archivo.
+    if OPTION=="r": #Comprueba si el argumento OPTION es igual al caracter 'r'.
+        return json_data["FLAG"] #Retorna el value de la llave "FLAG".
+    elif OPTION=="w": #Comprueba si el argumento OPTION es igual al caracter 'w'.
+        json_data["FLAG"]=False #Se establece el value de la llave "FLAG" en false.
+        with open(json_path, "w") as json_file: #Se abre el archivo json.
+            json.dump(json_data, json_file) #Se escribe en el archivo json.
     else:
         pass
+#***************************************************************************************************************
--- main.py
+++ main.py
@@ -5,87 +5,88 @@ from Modules.functions import *
 from Modules.constants import *
 
 if __name__ == '__main__':
-    date_log=str(datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d"))
-    if Flag_Status('r'):    
+    date_log=str(datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d")) #Se recoge el dato de fecha y hora en el instante en que se ejecuta el codigo.
+    if Flag_Status('r'): #Se consulta el estado de la bandera "FLAG" en el archivo json.    
+    #Si el estado es true se ejecuta el proceso.
         try:
-            logs_List=Download_Logs(date_log)
-            #logs_Path="C:/Users/mgarcia/Downloads/logs/"
-            #logs_List=os.listdir(logs_Path)
-            print(f"Log list: {logs_List}")
-            postgresql=psycopg2.connect(data_base_connect)
-            curpsql=postgresql.cursor()
-            for file_path in logs_List:
+            logs_List=Download_Logs(date_log) #Se recoge la lista de los archivos logs descargados que entrega la funcion Download_Logs.
+            print(f"Log list: {logs_List}") #Se imprime en pantalla la lista de los logs.
+            postgresql=psycopg2.connect(data_base_connect) #Se establece conexion con la base de datos.
+            curpsql=postgresql.cursor() #Se activa el cursor en la base de datos.
+            for file_path in logs_List: #Se itera dentro de la lista de logs selecionando uno por uno.
                 beginning=time.time()
-                with gzip.open(f'{file_path}', 'rt') as file:
-                    for line in file:
-                        quantity+=1
-                        if '#' in line:
+                with gzip.open(f'{file_path}', 'rt') as file: #Se abre el archivo para acceder a los datos.
+                    for line in file: #Se itera sobre el archivo recogiendo los datos linea a linea.
+                        quantity+=1 #Contador de lineas procesadas.
+                        if '#' in line: #Si en la linea selecionada se encuentra el caracter '#' se ignora.
                             pass
                         else:
-                            columns=line.split('\t')
-                            Uri=columns[7].split('/')
-                            Status_Validate='200'==columns[8] or '206'==columns[8]
-                            Query_Validate=not(columns[11]=='-') and len(columns[11].split('&'))==3
-                            Manifest_Validate='index.m3u8'== Uri[-1] or 'index.mpd'==Uri[-1] or 'Manifest'== Uri[-1]
-                            if Manifest_Validate and Status_Validate  and Query_Validate:
-                                SQL="INSERT INTO manifests VALUES(%s,%s,%s);"
-                                DATA=(
-                                    columns[0]+' '+columns[1],      # datetime
-                                    re.search(r"&Signature=(.{20}).*", columns[11]).groups()[0], # signature
-                                    profile[Uri[-1]],               # Segment Duration
+                            columns=line.split('\t') #Se crea una lista con cada dato en la linea separado por tabulacion.
+                            Uri=columns[7].split('/') #Los datos de la lista columns en la posicion 8 se separan por el caracter '/' y se crea una lista con los datos separados.
+                            Status_Validate='200'==columns[8] or '206'==columns[8] #Se valida que el dato de la lista columns en la posicion 9 corresponda con el esperado. 
+                            Query_Validate=columns[11]!='-' #Se valida que el dato de la lista columns en la posicion 12 no este vacio.
+                            Manifest_Validate='index.m3u8'== Uri[-1] or 'index.mpd'==Uri[-1] or 'Manifest'== Uri[-1] #Se valida con el dato de la lista Uri en la utima posicion corresponda con lo esperado. Esto identifica si los datos corresponden a un manifest.
+                            if Manifest_Validate and Status_Validate  and Query_Validate: #Condicion que evalua si todas lar variables son true.
+                                SQL="INSERT INTO manifests VALUES(%s,%s,%s);" #Sentencia SQL para insertar datos en la tabla mnifests de la base de datos
+                                #Datos a registrar con la sentencia anterior.
+                                DATA=( 
+                                    columns[0]+' '+columns[1],                                      # datetime
+                                    re.search(r"&Signature=(.{20}).*", columns[11]).groups()[0],    # signature
+                                    profile[Uri[-1]],                                               # Segment Duration
                                     )
-                                curpsql.execute(SQL,DATA)
-                                count_newmanifest+=1
+                                curpsql.execute(SQL,DATA) #Se envia a la base de datos la sentencia a ejecutar.
+                                count_newmanifest+=1 #Contador de manifest registrados
                             else: 
-                                Segment_Validate= re.search(r"index_video_\d\d?_\d_(\d+).mp4", Uri[-1]) or re.search(r"Fragments\(v=(\d+)\)", Uri[-1]) or re.search(r"index_\d_(\d+).ts", Uri[-1])
-                                if Status_Validate and Query_Validate and Segment_Validate:
-                                    SQL="INSERT INTO segments VALUES(%s, %s, %s);"
+                                Segment_Validate= re.search(r"index_video_\d\d?_\d_(\d+).mp4", Uri[-1]) or re.search(r"Fragments\(v=(\d+)\)", Uri[-1]) or re.search(r"index_\d_(\d+).ts", Uri[-1]) #Se recoge el valor del dato en la lista Uri en la utima posicion segun la coinsidencia.
+                                if Status_Validate and Query_Validate and Segment_Validate: #Condicion que evalua si todas las varibles son true.
+                                    SQL="INSERT INTO segments VALUES(%s, %s, %s);" #Sentencia SQL para insertar datos en la tabla segements de la base de datos.
+                                    #Datos a registrar con la sentencia anterior
                                     DATA=(
-                                        columns[0]+' '+columns[1],      # datetime
+                                        columns[0]+' '+columns[1],                                  # datetime
                                         re.search(r"&Signature=(.{20}).*",columns[11]).groups()[0], # signature
-                                        Segment_Validate.groups()[0],                # segmento
+                                        Segment_Validate.groups()[0],                               # segmento
                                         )    
-                                    curpsql.execute(SQL, DATA)
-                                    count_newsegments+=1
-                                    #postgresql.commit()
-                postgresql.commit()
-                finish=time.time()
-                dict_summary['Log_summary']=file_path.split('/')[-1]
-                dict_summary['Lines_processed']=str(quantity)
-                dict_summary['New_manifests_Registered']=str(count_newmanifest)
-                dict_summary['New_segments_Registered']=str(count_newsegments)
-                dict_summary['Process_duration']=str(round((finish-beginning),3))
-                dict_summary_srt=json.dumps(dict_summary, sort_keys=False, indent=8)
-                print_log("a", dict_summary_srt, date_log)
-                os.remove(file_path)
-                dict_summary={}
-                count_newmanifest=0
+                                    curpsql.execute(SQL, DATA) #Se envia a la base de datos la sentencia a ejecutar.
+                                    count_newsegments+=1 #Contador de segmentos registrados.
+                postgresql.commit() #Se guarda los cambios realizados en la base de datos.
+                finish=time.time() #Captura del tiempo en el instante que termina de procesar un log.
+                dict_summary['Log_summary']=file_path.split('/')[-1] #Se agrega al resumen el nombre del log procesado.
+                dict_summary['Lines_processed']=str(quantity) #Se agrega al resumen la cantidad total de lineas procesadas.
+                dict_summary['New_manifests_Registered']=str(count_newmanifest) #Se agrega al resumen la cantidad total de manifests registrados.
+                dict_summary['New_segments_Registered']=str(count_newsegments) #Se agrega al resumen la cantidad total de segmentos registrados.
+                dict_summary['Process_duration']=str(round((finish-beginning),3)) #Se agrega al resumen el calculo de la duracion total de la ejecucion del codeigo.
+                dict_summary_srt=json.dumps(dict_summary, sort_keys=False, indent=8) #Se transforma el diccionario a formato texto.
+                print_log(dict_summary_srt, date_log) #Se registra en el log de eventos el resumen.
+                os.remove(file_path) #Se elimnina el log procesado del folde local.
+                dict_summary={} #se limpia el diccionario.
+                #Se establecen los contadores a cero.
+                count_newmanifest=0 
                 count_VmxSegments=0
                 count_newsegments=0
                 quantity=0
-            curpsql.close()
-            postgresql.close()
+            curpsql.close() #Se cierra la conexion con el cursor de la base de datos.
+            postgresql.close() #Se cierra la conexion con la base de datos.
         except:
-            finish=time.time()
-            Flag_Status("w")
-            curpsql.close()
-            postgresql.close() #Postgresql
-            error=sys.exc_info()[2]
-            errorinfo=traceback.format_tb(error)[0]
-            error2=str(sys.exc_info()[1])
-            dict_summary['Log_summary']=file_path.split('/')[-1]
-            dict_summary['Lines_processed']=str(quantity)
-            dict_summary['Process_duration']=str(round((finish-beginning),3))
+            finish=time.time() #Captura del tiempo en el instante que termina de procesar un log.
+            Flag_Status("w") #Se cambia el estado de la bandera "FLAG" a false.
+            curpsql.close() #Se cierra la conexion con el cursor de la base de datos.
+            postgresql.close() #Se cierra la conexion con la base de datos.
+            error=sys.exc_info()[2] #Captura del error generado por el sistema.
+            errorinfo=traceback.format_tb(error)[0] #Cartura del detalle del error.
+            dict_summary['Log_summary']=file_path.split('/')[-1] #Se agrega al resumen el nombre del log procesado.
+            dict_summary['Lines_processed']=str(quantity) #Se agrega al resumen la cantidad total de lineas procesadas.
+            dict_summary['Process_duration']=str(round((finish-beginning),3)) #Se agrega al resumen el calculo de la duracion total de la ejecucion del codigo.
+            #Se agrega al resumen detalle del error ocurrido.
             dict_summary['Log_Error']={
                 'Error': str(sys.exc_info()[1]),
                 'error_info': errorinfo
             }
-            dict_summary_srt=json.dumps(dict_summary, sort_keys=False, indent=8)
-            print_log("a", dict_summary_srt, date_log)
-            mail_subject='Error DataAnaylitics'
-            SendMail(dict_summary_srt, mail_subject)
+            dict_summary_srt=json.dumps(dict_summary, sort_keys=False, indent=8) #Se transforma el diccionario a formato texto.
+            print_log(dict_summary_srt, date_log) #Se registra en el log de eventos el resumen.
+            mail_subject='Error DataAnaylitics' #Se establece el asunto del correo.
+            SendMail(dict_summary_srt, mail_subject) #Se envia correo electronico.
     else:
-        text_print="awsupload application failure not recognized\n"
-        print_log("a", text_print, date_log)
-        quit()
+        text_print="awsupload application failure not recognized\n" #Texto a imprimir o registrar.
+        print_log(text_print, date_log) #Se registra el texto anterior en el log de eventos.
+        quit() #Termina la ejecucion del codigo.
     
